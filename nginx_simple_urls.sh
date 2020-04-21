c027dfc0717c175680c30098f09d40c8f6fc0c72
Jithin PP <jithinp@microsoft.com>
2020-04-21 23:37:55 +0400

    Customize nginx home page with Server name

--- nginx_simple_urls.sh
+++ nginx_simple_urls.sh
@@ -1,6 +1,7 @@
 apt-get install -y nginx
-echo "<h1>Web Server - " $(hostname) "</h1>" | sudo tee /var/www/html/index.html
+server=$(hostname)
+sudo sed -i "s/Welcome to nginx/Web Server: $server/g" /var/www/html/index.html
 sudo mkdir -p /var/www/html/images
-echo "<h1>Images: " $(hostname) "</h1>" | sudo tee /var/www/html/images/test.html
+echo "<h1>Images URL: " $(hostname) "</h1>" | sudo tee /var/www/html/images/test.html
 sudo mkdir -p /var/www/html/video
-echo "<h1>Videos: " $(hostname) "</h1>" | sudo tee /var/www/html/video/test.html
+echo "<h1>Videos URL: " $(hostname) "</h1>" | sudo tee /var/www/html/video/test.html
