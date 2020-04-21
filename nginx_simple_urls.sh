1bce6b330ebebaaa54dc4836845c54a467f54ebc
Jithin PP <jithinp@microsoft.com>
2020-04-21 23:13:28 +0400

    Updated html formatting

--- nginx_simple_urls.sh
+++ nginx_simple_urls.sh
@@ -1,6 +1,6 @@
 apt-get install -y nginx
-echo "Web Server - " $(hostname) | sudo tee /var/www/html/index.html
+echo "<h1>Web Server - " $(hostname) "</h1>" | sudo tee /var/www/html/index.html
 sudo mkdir -p /var/www/html/images
-echo "Images: " $(hostname) | sudo tee /var/www/html/images/test.html
+echo "<h1>Images: " $(hostname) "</h1>" | sudo tee /var/www/html/images/test.html
 sudo mkdir -p /var/www/html/video
-echo "Video: " $(hostname) | sudo tee /var/www/html/video/test.html
+echo "<h1>Videos: " $(hostname) "</h1>" | sudo tee /var/www/html/video/test.html
