1fa7bbd34cf3712c8219c2a1e4b7a50d544fbced
Jithin PP <jithinp@microsoft.com>
2020-04-21 23:53:11 +0400

    Update nginx_simple_urls.sh
    minor fix

--- nginx_simple_urls.sh
+++ nginx_simple_urls.sh
@@ -1,5 +1,6 @@
 apt-get install -y nginx
 server=$(hostname)
+sudo cp /var/www/html/index.nginx-debian.html /var/www/html/index.html
 sudo sed -i "s/Welcome to nginx/Web Server: $server/g" /var/www/html/index.html
 sudo mkdir -p /var/www/html/images
 echo "<h1>Images URL: " $(hostname) "</h1>" | sudo tee /var/www/html/images/test.html
